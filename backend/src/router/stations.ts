0cbf4857b9954e765d2e5c7ef0b5a54d193064ba
vilsuo <vilsuo1@gmail.com>
2024-05-29 17:23:30 +0300

    calculated station summary

--- backend/src/router/stations.ts
+++ backend/src/router/stations.ts
@@ -1,10 +1,12 @@
 import { Router} from "express";
 import { Station } from "../entity/Station";
 import { AppDataSource } from "../datasource";
+import { Journey } from "../entity/Journey";
 
 const router = Router();
 
 const stationRepository = AppDataSource.getRepository(Station);
+const journeyRepository = AppDataSource.getRepository(Journey);
 
 router.get("/", async (_req, res) => {
   try {
@@ -29,7 +31,31 @@ router.get("/:id", async (req, res) => {
       return res.status(404).send({ message : "Station does not exist" })
     }
 
-    return res.status(200).send(station);
+    // starting
+    const journeysStarting = await journeyRepository
+      .findBy({ departureStationId: station.id });
+
+    const journeysStartingCount = journeysStarting.length;
+
+    const totalDistance = journeysStarting.reduce(
+      (prev, journey) => prev + (journey.distance || 0), 0
+    );
+
+    const totalDuration = journeysStarting.reduce(
+      (prev, journey) => prev + (journey.duration || 0), 0
+    );
+
+    // ending
+    const journeysEndingCount = await journeyRepository
+      .countBy({ returnStationId: station.id })
+
+    return res.status(200).send({
+      station,
+      starting: journeysStartingCount,
+      averageDistance: journeysStartingCount ? (totalDistance / journeysStartingCount) : 0,
+      averageDuration: journeysStartingCount ? (totalDuration / journeysStartingCount) : 0,
+      ending: journeysEndingCount,
+    });
 
   } catch (error: unknown) {
     return res.status(500).send({ error });
