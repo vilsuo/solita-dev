9fbca2e76bd8d56320e5cdd5f8d70631863510a7
vilsuo <vilsuo1@gmail.com>
2024-05-30 11:41:49 +0300

    serve static files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import express from "express";
+import path from "path";
 import { AppDataSource } from "./datasource";
 import router from "./router";
 
 const server = express();
 
+server.use(express.static(path.join(__dirname, "..", "dist")));
+
 server.get("/", (req, res) => {
-  return res.send("ok");
+  return res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
 server.use("/api", router);
